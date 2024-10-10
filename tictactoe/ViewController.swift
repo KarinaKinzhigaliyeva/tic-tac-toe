2559c5c80aa23bcdd90061edd72d82447e3b4fda
Karina Kinzhigaliyeva <karinakinzhigaliyeva@gmail.com>
2024-10-10 22:10:40 +0500

    Initial Commit

---
+++ tictactoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by Karina Kinzhigaliyeva on 10.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
